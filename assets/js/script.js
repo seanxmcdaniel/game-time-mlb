eac89f6fad82fc08f0a9d002527c9856682501ff
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-05-13 20:52:38 -0400

    Added pseudocode to JS and Teams list to HTML

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// PSEUDO CODE HERE:
+// 1. Set the APIs and API keys as variables
+// 2. MLB teams as array
+// 3. Create function to save chosen team to local storage
+// 4. When clicking favorite team, fetch schedule of team for TODAY from API
+// 5. Append schedule of team to new div
+// 6. Create function to call CURRENT stats of chosen team from API 
+// 7. Append stats as <li> to <ul> in the HTML
