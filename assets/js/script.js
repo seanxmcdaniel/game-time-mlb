c94094b62e038efc5f2f0c51bfd1a26aacacca2b
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-05-16 23:23:47 -0400

    Trying Moment.js to show only certain schedules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 var myScheduleKey = "?key=a3812c15735a4f739f3d2de60c5fd0bd";
-var scheduleApi = "https://api.sportsdata.io/v3/mlb/scores/json/Games/" // + {season}
-var selectTeam = document.getElementById('team-select')
-var selectTeamValue = selectTeam.options[selectTeam.selectedIndex].selectTeamValue;
+var scheduleApi = "https://api.sportsdata.io/v3/mlb/scores/json/Games/2022"
 var scheduleList = document.querySelector('.scheduleList')
 var scheduleBtn = document.querySelector('.show-schedule')
+var selectTeam = document.getElementById('team-select')
+var selectTeamValue = selectTeam.options[selectTeam.selectedIndex].selectTeamValue;
 
-fetch(scheduleApi+ '2022' + myScheduleKey)
+fetch(scheduleApi + myScheduleKey)
   .then(response => response.json())
   .then(data => console.log(data));
 
 function fetchSchedule() {
-  fetch(scheduleApi+ '2022' + myScheduleKey)
+  fetch(scheduleApi + myScheduleKey)
   .then(function(response) {
     return response.json();
   })
@@ -18,15 +18,14 @@ function fetchSchedule() {
     for (var i = 0; i < data.length; i++) {
       var listItem = document.createElement('li');
       if(data[i].AwayTeam === selectTeam.value){
-        listItem.textContent = data[i].AwayTeam + ' at ' + data[i].HomeTeam;
+        listItem.textContent = data[i].AwayTeam + ' at ' + data[i].HomeTeam + ' ' + data[i].Day;
         scheduleList.appendChild(listItem);
       } else if(data[i].HomeTeam === selectTeam.value) {
-        listItem.textContent = data[i].AwayTeam + ' at ' + data[i].HomeTeam;
+        listItem.textContent = data[i].AwayTeam + ' at ' + data[i].HomeTeam + ' ' + data[i].Day;
         scheduleList.appendChild(listItem);
-      }
-
+      } 
     }
   })
 };
 
-scheduleBtn.addEventListener('click', fetchSchedule);
+$('.show-schedule').click(fetchSchedule);
