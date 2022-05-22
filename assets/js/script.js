2451ba6f63ea737c43bc09feda51dfa7e2a611ab
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-05-21 22:20:23 -0400

    updates js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,23 @@
 var myScheduleKey = "?key=a3812c15735a4f739f3d2de60c5fd0bd";
 var scheduleApi = "https://api.sportsdata.io/v3/mlb/scores/json/Games/2022"
-var broadApi = "http://lookup-service-prod.mlb.com/json/named.mlb_broadcast_info.bam?src_type='TV'&src_comment='National'&tcid=mm_mlb_schedule&sort_by='game_time_et_asc'&home_away='H'";
 var scheduleList = document.querySelector('.scheduleList')
-var broadList = document.querySelector('.broadList')
-var selectTeam = document.getElementById('team-select')
+var standingsList = document.querySelector('.standings-list')
+const selectTeam = document.getElementById('team-select')
 var currentDay = moment().format("YYYY-MM-DDThh:mm:ss")
 var broadDate = moment().format("dd/MM/yyyy")
 var savedTeam = document.getElementById('savedTeam')
 var broadBtn = document.querySelector('.watch-game')
 
-// When document loads, get favorite team from local storage and 
-// put the value inside of the dropdown menu.
-$('document').ready(function () {
-  for (let i = 0; i < localStorage.length; i++) {
-    const favTeam = localStorage.favTeam;
 
-    savedTeam.innerHTML += favTeam;
-  }
-  // For our developing purposes, console log current day and favorite team
-  console.log(currentDay)
-  console.log(savedTeam.value)
+// Function to set full name of team to save on page on refresh instead of abbreviation
+selectTeam.addEventListener('change', (event) => {
+  let favoriteTeam = event.target.value;
+  localStorage.setItem('Team', favoriteTeam);
 });
+
+if ( localStorage.getItem('Team')) {
+  selectTeam.value = localStorage.getItem('Team'); 
+}
 
 // Fetches schedule API, returns JSON
 function fetchSchedule() {
@@ -44,27 +41,22 @@ function fetchSchedule() {
         }}
     })
   // Save selected team (value of the select in HTML) to local storage
-  localStorage.setItem('favTeam', selectTeam.value)
   console.log(localStorage)
 };
 
-function fetchBroadcast() {
-  fetch(broadApi + "&start_date='" + broadDate + "'" + "&end_date='" + broadDate + "'" + "&season='2022'")
-    .then(function (response) {
-      return response.json();
-    })
+function fetchStats() {
+  fetch('https://statsapi.mlb.com/api/v1/standings?leagueId=103&season=2022&standingsTypes=wildCard,regularSeason&hydrate=teammlb')
+	.then(function(response) {
+    return response.json()
+  })
     // Creates <li> for each game time, and appends the broadcast data to a new <li>.
     .then(function (data) {
       for (var i = 0; i < data.length; i++) {
-        var broadInfo = document.createElement('li');
-        var team = localStorage.getItem('favTeam')
+        var statsInfo = document.createElement('li');
         // Conditionals for choosing only the broadcast for selected teams.
-        if (data[i].away_team_abbrev === team) {
-          broadInfo.textContent = 'You can watch' + data[i].away_team_full + 'play against' + data[i].home_team_full + 'today at' + data[i].game_time_away + 'on' + data[i].source_desc + ".";
-          broadList.appendChild(broadInfo);
-        } else if (data[i].home_team_abbrev === team) {
-          broadInfo.textContent = 'You can watch' + data[i].home_team_full + 'play against' + data[i].away_team_full + 'today at' + data[i].game_time_home + 'on' + data[i].source_desc + ".";
-          broadList.appendChild(broadInfo);
+        if (data[i].divisionRank === 1) {
+          statsInfo.textContent = date[i].name + 'Division Rank:' + data[i].divisionRank + 'Wins:' + data[i].wins + 'Losses:' + data[i].losses;
+          standingsList.appendChild(statsInfo);
         }
       }
     }
@@ -78,3 +70,5 @@ $('.show-schedule').click(function () {
   localStorage.clear()
 }, fetchSchedule);
 // Then fetch new schedule and set new favorite team
+
+$('standingBtn').click(fetchStats);
