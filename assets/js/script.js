a1051ade4c500df94e8d93a5501270dc0acbff9e
Sean <seanmcdaniel93@gmail.com>
2022-05-22 22:17:25 -0400

    Final Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
 var myScheduleKey = "?key=a3812c15735a4f739f3d2de60c5fd0bd";
-var scheduleApi = "https://api.sportsdata.io/v3/mlb/scores/json/Games/2022"
-var statsApi = 'https://api.sportsdata.io/v3/mlb/scores/json/Standings/2022?key=a3812c15735a4f739f3d2de60c5fd0bd'
+var scheduleApi = "https://api.sportsdata.io/v3/mlb/scores/json/Games/2022";
+var statsApi = 'https://api.sportsdata.io/v3/mlb/scores/json/Standings/2022?key=a3812c15735a4f739f3d2de60c5fd0bd';
 var scheduleList = document.querySelector('.scheduleList')
 var standingsList = document.querySelector('.standings-list')
-const selectTeam = document.getElementById('team-select')
 var currentDay = moment().format("YYYY-MM-DDThh:mm:ss")
 var broadDate = moment().format("dd/MM/yyyy")
 var savedTeam = document.getElementById('savedTeam')
 var broadBtn = document.querySelector('.watch-game')
-
+const selectTeam = document.getElementById('team-select')
 
 // Function to set full name of team to save on page on refresh instead of abbreviation
 selectTeam.addEventListener('change', (event) => {
@@ -33,7 +32,7 @@ function fetchHome() {
         var listItem = document.createElement('li');
         // Conditionals for choosing only the schedule for selected teams.
         if (data[i].HomeTeam === selectTeam.value) {
-          listItem.textContent = data[i].AwayTeam + ' at ' + data[i].HomeTeam + ' Date and Time: ' + data[i].DateTime;
+          listItem.textContent = data[i].AwayTeam + ' at ' + data[i].HomeTeam + ' Date and Time: ' + moment(data[i].DateTime).format('MM/DD/YYYY h:mma');
           scheduleList.appendChild(listItem);
         } else if (data[i].DateTime < currentDay) {
           $('li').remove();
@@ -48,19 +47,16 @@ function fetchAway() {
     .then(function (response) {
       return response.json();
     })
-    // Creates <li> for each game time, and appends the schedule data to a new <li>.
     .then(function (data) {
       for (var i = 0; i < data.length; i++) {
         var listItem = document.createElement('li');
-        // Conditionals for choosing only the schedule for selected teams.
         if (data[i].AwayTeam === selectTeam.value) {
-          listItem.textContent = data[i].AwayTeam + ' at ' + data[i].HomeTeam + ' Date and Time: ' + data[i].DateTime;
+          listItem.textContent = data[i].AwayTeam + ' at ' + data[i].HomeTeam + ' Date and Time: ' + moment(data[i].DateTime).format('MM/DD/YYYY h:mma');
           scheduleList.appendChild(listItem);
         } else if (data[i].DateTime < currentDay) {
           $('li').remove();
         }}
     })
-  // Save selected team (value of the select in HTML) to local storage
   console.log(localStorage)
 };
 
@@ -69,17 +65,14 @@ function fetchStats() {
     .then(function (response) {
       return response.json();
     })
-    // Creates <li> for each game time, and appends the schedule data to a new <li>.
     .then(function (data) {
       for (var i = 0; i < data.length; i++) {
         var standingsItem = document.createElement('li');
-        // Conditionals for choosing only the schedule for selected teams.
         if (data[i].Key === selectTeam.value) {
           standingsItem.textContent = 'Division Rank: ' + data[i].DivisionRank + ' Wins: ' + data[i].Wins + ' Losses: ' + data[i].Losses;
           standingsList.appendChild(standingsItem);
         }}
     })
-  // Save selected team (value of the select in HTML) to local storage
   console.log(localStorage)
 };
 
@@ -92,14 +85,10 @@ $('.show-home').click(function () {
 }, fetchHome);
 // Then fetch new schedule and set new favorite team
 
-$('.show-away').click(fetchHome);
-
-// On second click, clear local storage and remove previous schedule
 $('.show-away').click(function () {
   $('li').remove()
   localStorage.clear()
 }, fetchAway);
-// Then fetch new schedule and set new favorite team
 
 $('.standingBtn').click(function () {
   $('li').remove();
